c4ce1b21524d186fa50cac05e708320940640fa9
Valentin Michalak <valentinmichalak@gmail.com>
2016-10-28 11:36:08 +0200

    Init commit

---
+++ src/main/java/com/waves/ikb4stream/model/LatLong.java
@@ -0,0 +1,51 @@
+package com.waves.ikb4stream.model;
+
+import java.util.Objects;
+
+public class LatLong {
+    private final double latitude;
+    private final double longitude;
+
+    public LatLong(double latitude, double longitude) {
+        Objects.requireNonNull(latitude);
+        Objects.requireNonNull(longitude);
+
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LatLong latLong = (LatLong) o;
+
+        if (Double.compare(latLong.latitude, latitude) != 0) return false;
+        return Double.compare(latLong.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(latitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return latitude + ", " + longitude;
+    }
+}
