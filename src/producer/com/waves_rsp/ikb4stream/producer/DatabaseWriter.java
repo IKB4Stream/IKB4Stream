fcc0507119d9914b78142ddcabd4ea82ec21e6e0
Valentin Michalak <valentinmichalak@gmail.com>
2017-02-05 17:05:24 +0100

    Add Database Writer

---
+++ src/producer/com/waves_rsp/ikb4stream/producer/DatabaseWriter.java
@@ -0,0 +1,49 @@
+package com.waves_rsp.ikb4stream.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.mongodb.async.client.MongoClient;
+import com.mongodb.async.client.MongoClients;
+import com.mongodb.async.client.MongoCollection;
+import com.mongodb.async.client.MongoDatabase;
+import com.waves_rsp.ikb4stream.core.model.Event;
+import org.bson.Document;
+
+public class DatabaseWriter {
+    private final MongoClient mongoClient;
+    private final MongoDatabase mongoDatabase;
+    private final MongoCollection<Document> mongoCollection;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private DatabaseWriter(MongoClient mongoClient, MongoDatabase mongoDatabase, MongoCollection mongoCollection) {
+        this.mongoClient = mongoClient;
+        this.mongoDatabase = mongoDatabase;
+        this.mongoCollection = mongoCollection;
+    }
+
+    /**
+     * Start a connection to the MongoDB Server
+     * @param connectionString the MongoDB Connection String
+     * @param databaseName the MongoDB database Name
+     * @param collectionName the MongoDB collection Name
+     * @return DatabaseWriter
+     */
+    public static DatabaseWriter connect(String connectionString, String databaseName, String collectionName) {
+        MongoClient mongoClient = MongoClients.create(connectionString);
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(databaseName);
+        MongoCollection mongoCollection = mongoDatabase.getCollection(collectionName);
+        return new DatabaseWriter(mongoClient, mongoDatabase, mongoCollection);
+    }
+
+    /**
+     * Insert One Event to the Database
+     * @param event
+     */
+    public void insertEvent(Event event) throws JsonProcessingException {
+        this.mongoCollection.insertOne(Document.parse(mapper.writeValueAsString(event)), (result, t) -> {
+            //ICI c'est quand l'event est envoyé
+        });
+    }
+
+}
