a64df09ecd0af26265f578d2d98bef1e8ab178d0
vh <hengvincent.1994@gmail.com>
2017-02-07 11:36:58 +0100

    SLF4J Logger integration

--- src/consumer/com/waves_rsp/ikb4stream/consumer/Main.java
+++ src/consumer/com/waves_rsp/ikb4stream/consumer/Main.java
@@ -1,9 +1,15 @@
 package com.waves_rsp.ikb4stream.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(com.waves_rsp.ikb4stream.producer.Main.class);
+
     public static void main(String[] args) {
         // Example of how to implement RDFParser & AnomalyRequest classes
         RDFParser rdfParser = new RDFParser();
         AnomalyRequest anomalyRequest = rdfParser.parse("resources/anomaly.ttl");
+        LOGGER.info("IKB4Stream Consumer Module start");
     }
 }
--- src/producer/com/waves_rsp/ikb4stream/producer/Main.java
+++ src/producer/com/waves_rsp/ikb4stream/producer/Main.java
@@ -1,7 +1,14 @@
 package com.waves_rsp.ikb4stream.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOGGER.info("IKB4Stream Producer Module start");
 
     }
 }
+
