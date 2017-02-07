2662621ccb2ebbe2e7f6e07ca6d9b155631f503f
vh <hengvincent.1994@gmail.com>
2017-02-07 16:02:54 +0100

    Fix Consumer Main, bad Main call

--- src/consumer/com/waves_rsp/ikb4stream/consumer/Main.java
+++ src/consumer/com/waves_rsp/ikb4stream/consumer/Main.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(com.waves_rsp.ikb4stream.producer.Main.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         // Example of how to implement RDFParser & AnomalyRequest classes
