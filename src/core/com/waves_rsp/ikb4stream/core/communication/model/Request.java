f8efe9cc2d53e338719a603e0d144a36022682ec
Boubacar <boubacar.dabo1@gmail.com>
2017-02-09 10:35:32 +0100

    Javadoc for the DataProducer

--- src/core/com/waves_rsp/ikb4stream/core/communication/model/Request.java
+++ src/core/com/waves_rsp/ikb4stream/core/communication/model/Request.java
@@ -3,12 +3,22 @@ package com.waves_rsp.ikb4stream.core.communication.model;
 import java.util.Date;
 import java.util.Objects;
 
+/**
+ * Request class which represents an anomaly
+ */
 public class Request {
     private final Date start;
     private final Date end;
     private final BoundingBox boundingBox;
     private final Date requestReception;
 
+    /**
+     * The request constructor
+     * @param start is the starting date of an anomaly
+     * @param end is the end date of an anomaly
+     * @param boundingBox coordinates
+     * @param requestReception is the reception date of the request
+     */
     public Request(Date start, Date end, BoundingBox boundingBox, Date requestReception) {
         Objects.requireNonNull(start);
         Objects.requireNonNull(end);
@@ -21,18 +31,34 @@ public class Request {
         this.requestReception = requestReception;
     }
 
+    /**
+     *
+     * @return the starting date of an anomaly
+     */
     public Date getStart() {
         return start;
     }
 
+    /**
+     *
+     * @return the end date of an anomaly
+     */
     public Date getEnd() {
         return end;
     }
 
+    /**
+     *
+     * @return the coordinates of the boundingbox
+     */
     public BoundingBox getBoundingBox() {
         return boundingBox;
     }
 
+    /**
+     *
+     * @return the reception date of the request
+     */
     public Date getRequestReceptionDate() {
         return requestReception;
     }
--- src/core/com/waves_rsp/ikb4stream/core/communication/model/Response.java
+++ src/core/com/waves_rsp/ikb4stream/core/communication/model/Response.java
@@ -6,10 +6,19 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Response class which represents the response of an anomaly
+ * @see Request which is an anomaly
+ */
 public class Response {
     private final List<Event> events;
     private final Request request;
 
+    /**
+     * The response constructor
+     * @param events list of events
+     * @param request an anomaly
+     */
     public Response(List<Event> events, Request request) {
         Objects.requireNonNull(events);
         Objects.requireNonNull(request);
@@ -18,10 +27,18 @@ public class Response {
         this.request = request;
     }
 
+    /**
+     *
+     * @return a list of events
+     */
     public List<Event> getEvents() {
         return Collections.unmodifiableList(events);
     }
 
+    /**
+     *
+     * @return an anomaly
+     */
     public Request getRequest() {
         return request;
     }
