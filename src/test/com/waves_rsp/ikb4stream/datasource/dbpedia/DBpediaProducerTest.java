03ce4204c9a45f284dbaed75f265058ee66cc18b
ASUSR-LOICK\Loick <renee.loick@gmail.com>
2017-02-13 22:32:07 +0100

    change sparql query for dbpedia connector and add some tests

--- src/test/com/waves_rsp/ikb4stream/datasource/dbpedia/DBpediaProducerTest.java
+++ src/test/com/waves_rsp/ikb4stream/datasource/dbpedia/DBpediaProducerTest.java
@@ -1,7 +1,8 @@
 package com.waves_rsp.ikb4stream.datasource.dbpedia;
 
-import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class DBpediaProducerTest {
 
@@ -11,12 +12,69 @@ public class DBpediaProducerTest {
         dBpediaProducerConnector.load(null);
     }
 
-    @Ignore
     @Test
     public void checkProducerResults() {
         DBpediaProducerConnector dBpediaProducerConnector = DBpediaProducerConnector.getInstance();
         dBpediaProducerConnector.load(dataProducer -> {
             //Do nothing
         });
+    }
+
+    @Test
+    public void checkIllegalArgument() {
+        try {
+            DBpediaProducerConnector producerConnector = DBpediaProducerConnector.getInstance();
+            producerConnector.load(dataProducer -> {
+                //Do nothing
+            });
+        }catch (IllegalArgumentException err) {
+            //Do nothing
+        }
+    }
+
+    @Test
+    public void checkThreadForDataProducer() {
+        final DBpediaProducerConnector producerConnector = DBpediaProducerConnector.getInstance();
+        Thread thread = new Thread(() -> {
+           producerConnector.load(dataProducer -> {
+               //Do nothing
+           });
+        });
+
+        thread.start();
+
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            //Do nothing
+        }finally {
+            thread.interrupt();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Test
+    public void checkThreadsPoolForDataProducer() {
+        Thread[] threads = new Thread[10];
+        for (int i=0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                DBpediaProducerConnector producerConnector = DBpediaProducerConnector.getInstance();
+                try {
+                    producerConnector.load(dataProducer -> {
+                        //Do nothing
+                    });
+                }catch (IllegalArgumentException err) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+
+        Arrays.stream(threads).forEach(Thread::start);
+
+        try {
+            Thread.sleep(300);
+        }catch (InterruptedException err) {
+            Arrays.stream(threads).forEach(Thread::interrupt);
+        }
     }
 }
