87dcb395ee2624db204255c6e0abf4265d11ef6f
vh <hengvincent.1994@gmail.com>
2017-02-14 10:46:10 +0100

    web comm with api request handled

--- src/communication/web/com/waves_rsp/ikb4stream/communication/web/VertxServer.java
+++ src/communication/web/com/waves_rsp/ikb4stream/communication/web/VertxServer.java
@@ -16,10 +16,17 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
+/**
+ * This server relies on Vertx, to handle the REST requests. It is instanciated by the web communication connector.
+ */
 public class VertxServer extends AbstractVerticle {
     private final Logger LOGGER = LoggerFactory.getLogger(VertxServer.class);
     private IDatabaseReader databaseReader = WebCommunication.databaseReader;
 
+    /**
+     * Server starting behaviour
+     * @param fut Future that handles the start status
+     */
     @Override
     public void start(Future<Void> fut) {
         Router router = Router.router(vertx);
@@ -42,28 +49,12 @@ public class VertxServer extends AbstractVerticle {
     }
 
     /**
-     * Read a request from a routing context, and attach the response to it. It requests the database with DatabaseReader.
+     * Reads a request from a routing context, and attach the response to it. It requests the database with DatabaseReader.
      * @param rc RoutingContext, which contains the request, and the response
      */
     private void getAnomalies(RoutingContext rc) {
         // curl http://localhost:8081/anomaly -X GET -H "Content-Type: application/json" -d '{"start":1487004295000,"end":1487004295000, "boundingBox":{"points": [{"latitude":10, "longitude":20},{"latitude":15, "longitude":25}]}, "requestReception":1487004295000}'
-        Date start = new Date(rc.getBodyAsJson().getLong("start"));
-        Date end = new Date(rc.getBodyAsJson().getLong("end"));
-        Date requestReception = new Date(rc.getBodyAsJson().getLong("requestReception"));
-
-        JsonObject jsonbb = rc.getBodyAsJson().getJsonObject("boundingBox");
-        JsonArray latlongs = jsonbb.getJsonArray("points");
-        LatLong[] ll = new LatLong[latlongs.size()];
-        for (int i=0; i < latlongs.size(); i++) {
-            JsonObject latlong = latlongs.getJsonObject(i);
-            double latitude = latlong.getDouble("latitude");
-            double longitude = latlong.getDouble("longitude");
-            ll[i] = new LatLong(latitude, longitude);
-            System.out.println(ll[i]);
-        }
-        BoundingBox bb = new BoundingBox(ll);
-
-        Request request = new Request(start, end, bb, requestReception);
+        Request request = parseRequest(rc.getBodyAsJson());
 
         rc.response().putHeader("content-type", "application/json");
 
@@ -72,7 +63,35 @@ public class VertxServer extends AbstractVerticle {
                 .end(jsonResponse.encode());
     }
 
+    /**
+     * Convert a request from Json to Java object
+     * @param jsonRequest json formatted request
+     * @return Java Request
+     */
+    private Request parseRequest(JsonObject jsonRequest) {
+        Date start = new Date(jsonRequest.getLong("start"));
+        Date end = new Date(jsonRequest.getLong("end"));
+        Date requestReception = new Date(jsonRequest.getLong("requestReception"));
 
+        JsonObject jsonbb = jsonRequest.getJsonObject("boundingBox");
+        JsonArray latlongs = jsonbb.getJsonArray("points");
+        LatLong[] ll = new LatLong[latlongs.size()];
+        for (int i=0; i < latlongs.size(); i++) {
+            JsonObject latlong = latlongs.getJsonObject(i);
+            double latitude = latlong.getDouble("latitude");
+            double longitude = latlong.getDouble("longitude");
+            ll[i] = new LatLong(latitude, longitude);
+        }
+        BoundingBox bb = new BoundingBox(ll);
+
+        return new Request(start, end, bb, requestReception);
+    }
+
+    /**
+     * Retrieve an event from database
+     * @param request the user web request
+     * @return a JSon Object extracted from the database
+     */
     private JsonObject getEvent(Request request) {
         String[] r = new String[1];
         databaseReader.getEvent(request, (t, result) -> {
