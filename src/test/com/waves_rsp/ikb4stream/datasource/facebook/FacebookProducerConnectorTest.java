d39c0e8dac369de51b21aafcfa87a468ae1ef6ca
Boubacar <boubacar.dabo1@gmail.com>
2017-02-15 17:21:13 +0100

    Facebook Producer Connector update

--- src/test/com/waves_rsp/ikb4stream/datasource/facebook/FacebookProducerConnectorTest.java
+++ src/test/com/waves_rsp/ikb4stream/datasource/facebook/FacebookProducerConnectorTest.java
@@ -1,16 +1,16 @@
 package com.waves_rsp.ikb4stream.datasource.facebook;
 
-import facebook.RestFBConnector;
+
 import org.junit.Test;
 
 /**
  * Created by ikb4stream on 14/02/17.
  */
-public class RestFBConnectorTest {
+public class FacebookProducerConnectorTest {
 
     @Test(expected = NullPointerException.class)
     public void testLoadNullDataProducer() {
-        RestFBConnector fb = new RestFBConnector();
+        FacebookProducerConnector fb = new FacebookProducerConnector();
         fb.searchWordFromGeolocation(null, 100,48.8781161065379, 2.22459235221894);
     }
 
