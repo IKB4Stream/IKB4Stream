405c1559a23011ab3e24403899b779bdca455ced
Boubacar <boubacar.dabo1@gmail.com>
2017-02-15 17:19:32 +0100

    Facebook Producer Connector update

--- src/datasource/facebook/com/waves_rsp/ikb4stream/datasource/facebook/FacebookProducerConnector.java
+++ src/datasource/facebook/com/waves_rsp/ikb4stream/datasource/facebook/FacebookProducerConnector.java
@@ -1,10 +1,14 @@
-package facebook;
+package com.waves_rsp.ikb4stream.datasource.facebook;
+
 import com.restfb.*;
 import com.restfb.types.Event;
+import com.waves_rsp.ikb4stream.core.datasource.model.IDataProducer;
+import com.waves_rsp.ikb4stream.core.datasource.model.IProducerConnector;
 import com.waves_rsp.ikb4stream.core.model.LatLong;
 import com.waves_rsp.ikb4stream.core.model.PropertiesManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,18 +16,26 @@ import java.util.Objects;
 
 
 /**
- * RestFBConnector class provides events link to a word form coordinates
+ * FacebookProducerConnector class provides events link to a word form coordinates
  */
-public class RestFBConnector {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RestFBConnector.class);
+public class FacebookProducerConnector implements IProducerConnector {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacebookProducerConnector.class);
     private final String source;
     private final String pageAccessToken;
+    private final String word;
+    private final int limit;
+    private final double lat;
+    private final double lon;
 
-    public RestFBConnector() {
+    public FacebookProducerConnector() {
         try {
-            PropertiesManager propertiesManager = PropertiesManager.getInstance(RestFBConnector.class, "resources/config.properties");
+            PropertiesManager propertiesManager = PropertiesManager.getInstance(FacebookProducerConnector.class, "resources/config.properties");
             this.source = propertiesManager.getProperty("FacebookProducerConnector.source");
             this.pageAccessToken = propertiesManager.getProperty("FacebookProducerConnector.token");
+            this.word =  propertiesManager.getProperty("FacebookProducerConnector.word");
+            this.limit =  Integer.valueOf(propertiesManager.getProperty("FacebookProducerConnector.limit"));
+            this.lat =  Double.valueOf(propertiesManager.getProperty("FacebookProducerConnector.latitude"));
+            this.lon =  Double.valueOf(propertiesManager.getProperty("FacebookProducerConnector.longitude"));
         } catch (IllegalArgumentException e) {
             LOGGER.error(e.getMessage());
             throw new IllegalArgumentException("Invalid configuration");
@@ -72,5 +84,21 @@ public class RestFBConnector {
                 && (event.getStartTime() != null)
                 && (event.getDescription() != null)
                 && (event.getEndTime() != null);
+    }
+
+    @Override
+    public void load(IDataProducer dataProducer) {
+        Objects.requireNonNull(dataProducer);
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                List<com.waves_rsp.ikb4stream.core.model.Event> events = searchWordFromGeolocation(word, limit, lat, lon);
+                if (events.size() != 0) {
+                    events.stream().forEach(e -> dataProducer.push(e));
+                }
+                Thread.sleep(20000);
+            }catch (InterruptedException e){
+                LOGGER.error(e.getMessage());
+            }
+        }
     }
 }
