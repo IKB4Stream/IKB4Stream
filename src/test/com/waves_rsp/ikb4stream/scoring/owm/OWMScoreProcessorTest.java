6e61f144241c695370d2fa3ef75828a04cb5d0ee
Boubacar <boubacar.dabo1@gmail.com>
2017-02-21 10:47:42 +0100

    Fixed unit test for OWM

--- src/test/com/waves_rsp/ikb4stream/scoring/owm/OWMScoreProcessorTest.java
+++ src/test/com/waves_rsp/ikb4stream/scoring/owm/OWMScoreProcessorTest.java
@@ -2,15 +2,31 @@ package com.waves_rsp.ikb4stream.scoring.owm;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 
 public class OWMScoreProcessorTest {
     private final OWMScoreProcessor tsp = new OWMScoreProcessor();
     private final String source = "OpenWeatherMap";
 
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = NullPointerException.class)
     public void nullProcessScore() {
         tsp.processScore(null);
     }
 
+    @Test
+    public void testMinValues(){
+        assertEquals(0, tsp.verifyMaxScore((byte) -50));
+    }
+
+    @Test
+    public void testMaxValues(){
+        assertEquals(100, tsp.verifyMaxScore((byte) 102));
+    }
+
+    @Test
+    public void testRightValues(){
+        assertEquals(50, tsp.verifyMaxScore((byte) 50));
+    }
 }
