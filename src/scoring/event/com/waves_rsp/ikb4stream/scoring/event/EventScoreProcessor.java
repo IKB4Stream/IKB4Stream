835020a160f1ef9d187af710abeb9d87f32b8c9a
d3vpasha <demir.mehmet90@gmail.com>
2017-02-22 15:49:26 +0100

    Creation of EventScoreProcessor which concerns DBPedia, LeParisienRSS & Facebook

---
+++ src/scoring/event/com/waves_rsp/ikb4stream/scoring/event/EventScoreProcessor.java
@@ -0,0 +1,71 @@
+package com.waves_rsp.ikb4stream.scoring.event;
+
+import com.waves_rsp.ikb4stream.core.datasource.model.IScoreProcessor;
+import com.waves_rsp.ikb4stream.core.metrics.MetricsLogger;
+import com.waves_rsp.ikb4stream.core.model.Event;
+import com.waves_rsp.ikb4stream.core.model.PropertiesManager;
+import com.waves_rsp.ikb4stream.core.util.RulesReader;
+import com.waves_rsp.ikb4stream.core.util.nlp.OpenNLP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+
+public class EventScoreProcessor implements IScoreProcessor {
+    private static final MetricsLogger METRICS_LOGGER = MetricsLogger.getMetricsLogger();
+    private static final PropertiesManager PROPERTIES_MANAGER = PropertiesManager.getInstance(EventScoreProcessor.class, "resources/scoreprocessor/event/config.properties");
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventScoreProcessor.class);
+    private final OpenNLP openNLP = OpenNLP.getOpenNLP(Thread.currentThread());
+    private final Map<String, Integer> rulesMap;
+
+    public EventScoreProcessor() {
+        try {
+            String ruleFilename = PROPERTIES_MANAGER.getProperty("event.rules.file");
+            rulesMap = RulesReader.parseJSONRules(ruleFilename);
+        } catch (IllegalArgumentException e) {
+            LOGGER.error(e.getMessage());
+            throw new IllegalStateException(e.getMessage());
+        }
+    }
+
+    /**
+     * Process score of an event from an Event
+     *
+     * @param event an event without score
+     * @return Event with a score after OpenNLP processing
+     * @throws NullPointerException if {@param event} is null
+     */
+    @Override
+    public Event processScore(Event event) {
+        Objects.requireNonNull(event);
+        long start = System.currentTimeMillis();
+        String content = event.getDescription();
+        List<String> eventList = openNLP.applyNLPlemma(content);
+        byte score = 0;
+        for (String word : eventList) {
+            if (rulesMap.containsKey(word)) {
+                score += rulesMap.get(word);
+            }
+        }
+        if (score > 100) {
+            score = 100;
+        }
+        long end = System.currentTimeMillis();
+        // TODO: send Long
+        METRICS_LOGGER.log("time_scoring_" + event.getSource(), String.valueOf(end-start));
+        return new Event(event.getLocation(), event.getStart(), event.getEnd(), event.getDescription(), score, event.getSource());
+    }
+
+    @Override
+    public List<String> getSources() {
+        List<String> sources = new ArrayList<>();
+        try {
+            String allSources = PROPERTIES_MANAGER.getProperty("event.scoring.sources");
+            sources.addAll(Arrays.asList(allSources.split(",")));
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn(e.getMessage());
+        }
+        return sources;
+    }
+}
