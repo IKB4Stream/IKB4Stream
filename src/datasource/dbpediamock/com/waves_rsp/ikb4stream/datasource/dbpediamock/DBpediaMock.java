fc2577575c4f6ebbcab8cc7073b78a0ed6883491
ASUSR-LOICK\Loick <renee.loick@gmail.com>
2017-02-23 11:42:14 +0100

    branch new_mock created gitlab yeah finally

--- src/datasource/dbpediamock/com/waves_rsp/ikb4stream/datasource/dbpediamock/DBpediaMock.java
+++ src/datasource/dbpediamock/com/waves_rsp/ikb4stream/datasource/dbpediamock/DBpediaMock.java
@@ -2,11 +2,18 @@ package com.waves_rsp.ikb4stream.datasource.dbpediamock;
 
 import com.waves_rsp.ikb4stream.core.datasource.model.IDataProducer;
 import com.waves_rsp.ikb4stream.core.datasource.model.IProducerConnector;
+import com.waves_rsp.ikb4stream.core.metrics.MetricsLogger;
+import com.waves_rsp.ikb4stream.core.model.PropertiesManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DBpediaMock implements IProducerConnector {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DBpediaMock.class);
+    private static final MetricsLogger METRICS_LOGGER = MetricsLogger.getMetricsLogger();
+    private static final PropertiesManager PROPERTIES_MANAGER = PropertiesManager.getInstance(DBpediaMock.class, "resources/datasource/dbpediamock/config.properties");
 
     public DBpediaMock() {
-        //Do nothing
+
     }
 
     @Override
@@ -16,6 +23,10 @@ public class DBpediaMock implements IProducerConnector {
 
     @Override
     public boolean isActive() {
-        return false;
+        try {
+            return Boolean.valueOf(PROPERTIES_MANAGER.getProperty("dbpediamock.enable"));
+        } catch (IllegalArgumentException e) {
+            return true;
+        }
     }
 }
