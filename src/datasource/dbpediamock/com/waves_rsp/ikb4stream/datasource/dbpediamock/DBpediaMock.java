cd3356c41f6e4dd535c352b245bc857f8dd67dc1
ASUSR-LOICK\Loick <renee.loick@gmail.com>
2017-02-23 11:49:12 +0100

    branch new_mock created

--- src/datasource/dbpediamock/com/waves_rsp/ikb4stream/datasource/dbpediamock/DBpediaMock.java
+++ src/datasource/dbpediamock/com/waves_rsp/ikb4stream/datasource/dbpediamock/DBpediaMock.java
@@ -7,17 +7,20 @@ import com.waves_rsp.ikb4stream.core.model.PropertiesManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
+
 public class DBpediaMock implements IProducerConnector {
     private static final Logger LOGGER = LoggerFactory.getLogger(DBpediaMock.class);
     private static final MetricsLogger METRICS_LOGGER = MetricsLogger.getMetricsLogger();
     private static final PropertiesManager PROPERTIES_MANAGER = PropertiesManager.getInstance(DBpediaMock.class, "resources/datasource/dbpediamock/config.properties");
 
     public DBpediaMock() {
-
+        //Do nothing
     }
 
     @Override
     public void load(IDataProducer dataProducer) {
+        Objects.requireNonNull(dataProducer);
 
     }
 
