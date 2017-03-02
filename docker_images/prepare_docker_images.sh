825012499da7c58cf208898e5bec9aab2cccb2fb
ikb4stream <mollen.kev@gmail.com>
2017-03-02 16:48:20 +0100

    Automatize docker images build in gitlab. Correct the script.

--- docker_images/prepare_docker_images.sh
+++ docker_images/prepare_docker_images.sh
@@ -12,7 +12,7 @@ PRODUCER_VERSION="latest"
 CONSUMER_JAR_NAME="ikb4stream-1.0-SNAPSHOT-consumer.jar"
 CONSUMER_VERSION="latest"
 
-RESOURCES_NAME="resources"
+RESOURCES_NAME="resources/"
 
 PRODUCER_IMAGE_FOLDER_PATH="docker_images/producer"
 CONSUMER_IMAGE_FOLDER_PATH="docker_images/consumer"
@@ -24,45 +24,20 @@ echo "#####################################################################"
 
 #go into the root path
 pwd
-cd $ROOT_PATH$DISTRI_PATH
+cd $ROOT_PATH"/"$DISTRI_PATH
 
 #unzip file
 echo "File unzipping..."
 unzip $ZIP_NAME
 echo "File unzipped!"
 
-#create folder to store images
-#echo "Image's folders creation..."
-#mkdir producer/
-#mkdir consumer/
-#echo "Folders created!"
-
 #files copies
 echo "Files copies..."
-cp $PRODUCER_JAR_NAME $ROOT_PATH$PRODUCER_IMAGE_FOLDER_PATH"/"
-cp $CONSUMER_JAR_NAME $ROOT_PATH$CONSUMER_IMAGE_FOLDER_PATH"/"
-cp -r $RESOURCES_NAME"/"S $ROOT_PATH$PRODUCER_IMAGE_FOLDER_PATH"/"$RESOURCES_NAME
-cp -r $RESOURCES_NAME"/"S $ROOT_PATH$CONSUMER_IMAGE_FOLDER_PATH"/"$RESOURCES_NAME
+cp $PRODUCER_JAR_NAME $ROOT_PATH"/"$PRODUCER_IMAGE_FOLDER_PATH"/"
+cp $CONSUMER_JAR_NAME $ROOT_PATH"/"$CONSUMER_IMAGE_FOLDER_PATH"/"
+cp -r $RESOURCES_NAME"/"S $ROOT_PATH"/"$PRODUCER_IMAGE_FOLDER_PATH"/"$RESOURCES_NAME
+cp -r $RESOURCES_NAME"/"S $ROOT_PATH"/"$CONSUMER_IMAGE_FOLDER_PATH"/"$RESOURCES_NAME
 echo "Files copied!"
-
-#producer image creation
-#echo "Creation of the producer image version : $PRODUCER_VERSION"
-#cd producer/
-#docker build -t producer:$PRODUCER_VERSION .
-#cd ..
-#echo "Producer image created!"
-
-#consumer image creation
-#echo "Creation of the consumer image version : $CONSUMER_VERSION"
-#cd consumer/
-#docker build -t consumer:$CONSUMER_VERSION .
-#cd ..
-#echo "Consumer image created!"
-
-#cleaning the folder
-#echo "Cleaning the folder"
-#rm *.jar
-#echo "Folder cleaned!"
 
 pwd
 ls
