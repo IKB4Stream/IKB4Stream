d7eba3495fe81a35060e50f8de9accfed71a6e19
Valentin Michalak <valentinmichalak@gmail.com>
2017-03-02 14:20:07 +0100

    Add beautiful logo w/ love

--- src/producer/com/waves_rsp/ikb4stream/producer/Main.java
+++ src/producer/com/waves_rsp/ikb4stream/producer/Main.java
@@ -57,12 +57,60 @@ public class Main {
      * @see Main#PRODUCER_MANAGER
      */
     public static void main(String[] args) {
+        printLogo();
         LOGGER.info("IKB4Stream Producer Module start");
         PRODUCER_MANAGER.instantiate();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             LOGGER.info("IKB4Stream Producer Module stop");
             PRODUCER_MANAGER.stop();
         }));
+    }
+    
+    private static final void printLogo() {
+        System.out.println(
+                "\n" +
+                "`////////////////////////////////////////////////////////////.\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddhoooydddddddddddddddooosdddddhsoooydddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd   `ddddo.  `/hdddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd   `dds-   :ydddddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd   `y:   -sdddddddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd    `   :dddddddddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd     `   /hdddddddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd    /y/   .yddddddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd   `ddds.  `+dddddddddddd+\n" +
+                "-ddddddddddddddds   /ddddddddddddddd   `ddddh:   :hdddddddddd+\n" +
+                "-dddddddddddddddhoooydddddddddddddddooosddddddsooosdddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddyooooooosyddddddddddddddddddddooooddddddddddddddd+\n" +
+                "-ddddddddddd-          -ydddddddddddddddh-   oddddddddddddddd+\n" +
+                "-ddddddddddd-   osso/   `ddddddddddddddy`  `ydddddddddddddddd+\n" +
+                "-ddddddddddd-   hdddh   .ddddddddddddds`  .hddddddddddddddddd+\n" +
+                "-ddddddddddd-   ...`   :hdddddddddddd+   -h```/dddddddddddddd+\n" +
+                "-ddddddddddd-   ----.` `:ydddddddddd/   `++   -oydddddddddddd+\n" +
+                "-ddddddddddd-   yddddh   .dddddddddd`           /dddddddddddd+\n" +
+                "-ddddddddddd-   ossso:   .ddddddddddsssssso   -sydddddddddddd+\n" +
+                "-ddddddddddd-          `:hddddddddddddddddh   :dddddddddddddd+\n" +
+                "-dddddddddddsoooooooosyhdddddddddddddddddddoooydddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                "-dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd+\n" +
+                ".oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo:\n" +
+                "\n" +
+                "\n" +
+                "           hh+\n" +
+                " `:osso/ `oddho+  +o+:o/  ./oss+-`   /osso/.  -oo:oso:`:oss+. \n" +
+                " +dh/:++` :dds:-  yddyo: +dd   hds`      ydh` /ddy+/hdhh+/ydh.\n" +
+                " -yhyso:`  ddo    ydh`  `ddhoooydd-  sssshdd. /dd-  /dd-  -dd:\n" +
+                " ..-:+dd/  dds`.  ydy    hdy        ddo  sdd. /dd.  /dd.  -dd:\n" +
+                "`shysshy.  +hdyh: ydy    .ohhyyhy.  `hhyyydd. /dd.  /dd.  -dd:\n" +
+                "  `.--.     `--.  ...      `.--.     `--.`..  `..   `..   `..`\n" +
+                "\n");
     }
 }
 
